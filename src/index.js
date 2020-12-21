94f35168b73bca5d68b4856342187b0f9dace74f
TheComeBackGuy <71109038+TheComeBackGuy@users.noreply.github.com>
2020-12-21 16:58:11 -0500

    changed approach Gave it another go with a different approach. Cycled backward through list.

--- src/index.js
+++ src/index.js
@@ -13,51 +13,19 @@ LeetCode Problems
  */
 
 var plusOne = function (digits) {
-  //function for the first number to != 0
-  const aboveZero = (num) => num > 0;
-  /***
- *  finds that number and returns it's index
-  which will coincidently be the amount of zeros
-  since the arrays start at zero
- */
-  let zeroCount = digits.findIndex(aboveZero);
-  //if the array can't find a num != 0...
-  if (zeroCount === -1) {
-    //...the array must be all zeros
-    zeroCount = digits.length;
-    // console.log(digits);
-    // return theNumber;
-    let theNumber = [1];
-
-    for (let i = 0; i < zeroCount - 1; i++) {
-      // console.log(i);
-      theNumber.splice(0, 0, 0);
+  let carryOver = 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += carryOver;
+      return digits;
+    } else {
+      digits[i] = 0;
+      if (i === 0) {
+        digits.unshift(1);
+        return digits;
+      }
     }
-    console.log(theNumber);
-    return theNumber;
-    //Otherwise...
-  } else {
-    console.log("there are " + zeroCount + " precfix zeros");
-    //theNumber first joins each index to a string...
-    //then converts that to an integer...
-    //then adds 1 to the total of that integer
-
-    // console.log(BigInt(digits.join("")));
-    const theNumber = (Number(digits.join("")) + 1)
-      //Then we convert back to a string...
-      .toString()
-      //split it back up..
-      .split("")
-      //then use map function to make everything an integer again.
-      .map(Number);
-    //if we found any leading zeros
-    for (let i = 0; i < zeroCount; i++) {
-      // console.log(i);
-      theNumber.splice(0, 0, 0);
-    }
-    console.log(theNumber);
-    return theNumber;
   }
 };
 
-plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4]);
+plusOne([9]);
